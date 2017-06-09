4e900d7f0fba0c3bb5ef3d119fa9d59c555d377c
Marco Roman <marcoroman77@yahoo.com>
2017-06-09 01:07:42 -0700

    Reworked board class

---
+++ src/Board.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+/**
+ * Created by marco on 6/8/2017.
+ */
+public class Board {
+    private char[][] board = new char[8][8];
+    private ArrayList<Board> children = new ArrayList<>();
+    private Board parent;
+
+    public Board(){parent = null;}
+
+    //Creates a copy of a parent move, then stores selected move
+    //Used for computer move
+    public Board(int row, int col, Board b){
+        parent = b;
+
+        for(int i = 0; i < 8; ++i)
+            board[i] = b.getBoard()[i].clone();
+
+        board[row][col] = 'X';
+    }
+
+    //Allows the user to enter a move on the board
+    public void userMove(int row, int col){
+        board[row][col] = 'O';
+    }
+
+    public char[][] getBoard(){
+        return board;
+    }
+
+    public Board getParent(){
+        return parent;
+    }
+
+    public boolean validMove(int row, int col){
+        return board[row][col] == '\u0000';
+    }
+}
